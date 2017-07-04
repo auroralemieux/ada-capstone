1b016937ee5fa400126be90b5d011c80b9b0b410
Aurora Lemieux <aurora.lemieux@gmail.com>
2017-07-04 12:18:53 -0700

    added upload_tree template

--- babynamebook/urls.py
+++ babynamebook/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
+    url(r'^upload_tree/$', views.upload_tree, name='upload_tree'),
 ]
--- babynamebook/views.py
+++ babynamebook/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'babynamebook/home.html', {})
+
+def upload_tree(request):
+    return render(request, 'babynamebook/upload_tree.html', {})
