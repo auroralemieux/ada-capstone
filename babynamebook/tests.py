317b882d922bc717478a73eea8d15cc5e3762cad
Aurora Lemieux <aurora.lemieux@gmail.com>
2017-07-05 10:21:21 -0700

    adding tests for models and views but not running yet

--- babynamebook/tests.py
+++ babynamebook/tests.py
@@ -3,5 +3,64 @@ from babynamebook.models import Book, Person, Name, User
 from django.utils import timezone
 from django.core.urlresolvers import reverse
 from babynamebook.forms import BookForm
+import unittest
+from selenium import webdriver
 
-# Create your tests here.
+# models test
+class BookTest(TestCase):
+    # don't know how to test the FileField
+    def create_book(self, title="test_book", upload_tree="WHAT GOES HERE??"):
+        return Book.objects.create(title=title, upload_tree=upload_tree)
+
+    # what is the unicode part?
+    def test_book_creation(self):
+        b = self.create_book()
+        self.assertTrue(isinstance(b, Book))
+        self.assertEqual(b.__unicode__(), b.title)
+
+
+    # views (uses reverse)
+
+    def test_home_view(self):
+        w = self.create_whatever()
+        url = reverse("whatever.views.whatever")
+        resp = self.client.get(url)
+
+        self.assertEqual(resp.status_code, 200)
+        self.assertIn(w.title, resp.content)
+
+
+
+class TestHome(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+
+    def test_home_chrome(self):
+        self.driver.get("http://127.0.0.1:8000/")
+        # self.driver.find_element_by_id('id_title').send_keys("test title")
+        # self.driver.find_element_by_id('id_body').send_keys("test body")
+        self.driver.find_element_by_id('start-button').click()
+        self.assertIn("http://127.0.0.1:8000/upload_tree", self.driver.current_url)
+
+    def tearDown(self):
+        self.driver.quit
+
+class TestUploadTree(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+
+    def test_home_chrome(self):
+        self.driver.get("http://127.0.0.1:8000/upload_tree")
+        self.driver.find_element_by_id('id_title').send_keys("test-book-title")
+        # what do I put in the file upload field to test it??
+        self.driver.find_element_by_id('id_tree_upload').send_keys("../media/uploads/aurora-tree.ged")
+        self.driver.find_element_by_id('start-button').click()
+        self.assertIn("http://127.0.0.1:8000/progress", self.driver.current_url)
+
+    def tearDown(self):
+        self.driver.quit
+
+if __name__ == '__main__':
+    unittest.main()
--- babynamebook/views.py
+++ babynamebook/views.py
@@ -4,7 +4,6 @@ from .utils import parse_ged, parse_xml
 from django.conf import settings
 from django.core.files.storage import FileSystemStorage
 from .models import Book, Person
-from django.contrib.sessions.backends.db import SessionStore
 
 def home(request):
     return render(request, 'babynamebook/home.html', {})
@@ -36,10 +35,8 @@ def upload_tree(request):
 
                 new_person.book = book
                 new_person.save()
-        # book_id = book.id
         request.session["book_id"] = book.id
         return redirect('progress')
-        # return redirect('home')
     else:
         form = BookForm()
     return render(request, 'babynamebook/upload_tree.html', {'form': form})
@@ -47,9 +44,8 @@ def upload_tree(request):
 def progress(request):
     book = get_object_or_404(Book, id=request.session["book_id"])
     persons = Person.objects.filter(book=book)
-    print(len(persons))
-    # for person in persons[0:2]:
-    #     print(person.first_name)
-    #     print(person.first_name.name)
+    total_persons = len(persons)
+    num_m = len(Person.objects.filter(book=book, gender="M"))
+    num_f = len(Person.objects.filter(book=book, gender="F"))
 
-    return render(request, 'babynamebook/progress.html', {'book': book, 'persons': persons})
+    return render(request, 'babynamebook/progress.html', {'book': book, 'persons': persons, 'total_persons': total_persons, 'num_m': num_m, 'num_f': num_f})
