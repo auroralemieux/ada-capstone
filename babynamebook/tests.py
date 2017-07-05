ea00c4f1a6bf7035d7bdb024bc542d901399f5ea
Aurora Lemieux <aurora.lemieux@gmail.com>
2017-07-05 09:31:48 -0700

    added coverage module and setting up tests.py file

--- babynamebook/tests.py
+++ babynamebook/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
+from babynamebook.models import Book, Person, Name, User
+from django.utils import timezone
+from django.core.urlresolvers import reverse
+from babynamebook.forms import BookForm
 
 # Create your tests here.
