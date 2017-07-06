81e35d366e7092cda93c846a615911f60f31b137
Aurora Lemieux <aurora.lemieux@gmail.com>
2017-07-06 07:38:52 -0700

    moved parsing from upload view to progress view

--- babynamebook/views.py
+++ babynamebook/views.py
@@ -24,28 +24,28 @@ def upload_tree(request):
             book = Book(tree_upload=request.FILES['tree_upload'], title=request.POST['title'])
             # add stuff about user
             book.save()
-            filename = book.tree_upload.name
-            xml_filename = parse_ged(filename)
-
-            # person_list is an array of dictionary objects
-            person_list = parse_xml(xml_filename)
-
-            for p in person_list:
-                new_person = Person(first_name=p["first_name"], last_name=p["last_name"], gender=p["sex"], birth_year=p["birth_year"], )
-                if p["first_name"] is None:
-                    continue
-
-                if p["last_name"] is None:
-                    new_person.last_name = "unknown"
-
-                if p["sex"] is None:
-                    new_person.gender = "x"
-
-                if p["birth_year"] is None:
-                    new_person.birth_year = "0000"
-
-                new_person.book = book
-                new_person.save()
+            # filename = book.tree_upload.name
+            # xml_filename = parse_ged(filename)
+            #
+            # # person_list is an array of dictionary objects
+            # person_list = parse_xml(xml_filename)
+            # parse_person(person_list)
+            # for p in person_list:
+            #     new_person = Person(first_name=p["first_name"], last_name=p["last_name"], gender=p["sex"], birth_year=p["birth_year"], )
+            #     if p["first_name"] is None:
+            #         continue
+            #
+            #     if p["last_name"] is None:
+            #         new_person.last_name = "unknown"
+            #
+            #     if p["sex"] is None:
+            #         new_person.gender = "x"
+            #
+            #     if p["birth_year"] is None:
+            #         new_person.birth_year = "0000"
+            #
+            #     new_person.book = book
+            #     new_person.save()
         request.session["book_id"] = book.id
         return redirect('progress')
     else:
@@ -55,9 +55,36 @@ def upload_tree(request):
 
 def progress(request):
     book = get_object_or_404(Book, id=request.session["book_id"])
+    filename = book.tree_upload.name
+    xml_filename = parse_ged(filename)
+
+    # person_list is an array of dictionary objects
+    person_list = parse_xml(xml_filename)
+    parse_person(request, person_list)
     persons = Person.objects.filter(book=book)
     total_persons = len(persons)
     num_m = len(Person.objects.filter(book=book, gender="M"))
     num_f = len(Person.objects.filter(book=book, gender="F"))
 
     return render(request, 'babynamebook/progress.html', {'book': book, 'persons': persons, 'total_persons': total_persons, 'num_m': num_m, 'num_f': num_f})
+
+
+# this is a private method
+def parse_person(request, list):
+    for p in list:
+        new_person = Person(first_name=p["first_name"], last_name=p["last_name"], gender=p["sex"], birth_year=p["birth_year"], )
+        if p["first_name"] is None:
+            continue
+
+        if p["last_name"] is None:
+            new_person.last_name = "unknown"
+
+        if p["sex"] is None:
+            new_person.gender = "x"
+
+        if p["birth_year"] is None:
+            new_person.birth_year = 0
+        book = get_object_or_404(Book, id=request.session["book_id"])
+
+        new_person.book = book
+        new_person.save()
