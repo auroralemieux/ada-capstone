fa6e288242285de4a06feed57a5479868845657e
Aurora Lemieux <aurora.lemieux@gmail.com>
2017-07-09 21:20:48 -0700

    passing test for book model string method

--- babynamebook/tests/test_models.py
+++ babynamebook/tests/test_models.py
@@ -14,14 +14,14 @@ class BookTest(TestCase):
     ## this is passing!!!!!
     def test_book_creation(self):
         file_mock = mock.MagicMock(spec=File, name='FileMock')
-        file_mock.name = 'test1.jpg'
+        file_mock.name = 'test1.ged'
 
         book = Book(title="test-book")
         book.upload_tree = file_mock
 
         storage_mock = mock.MagicMock(spec=Storage, name='StorageMock')
         storage_mock.url = mock.MagicMock(name='url')
-        storage_mock.url.return_value = '/tmp/test1.jpg'
+        storage_mock.url.return_value = '/tmp/test1.ged'
 
         with mock.patch('django.core.files.storage.default_storage._wrapped', storage_mock):
             book.save()
@@ -29,6 +29,19 @@ class BookTest(TestCase):
         self.assertTrue(isinstance(book, Book))
         self.assertEqual("test-book", book.title)
 
-    # def test_string_method(self):
-    #     b = self.create_book()
-    #     self.assertEqual(b.__string__(), b.title)
+    def test_string_method(self):
+
+        file_mock = mock.MagicMock(spec=File, name='FileMock')
+        file_mock.name = 'test1.ged'
+
+        book = Book(title="test-book")
+        book.upload_tree = file_mock
+
+        storage_mock = mock.MagicMock(spec=Storage, name='StorageMock')
+        storage_mock.url = mock.MagicMock(name='url')
+        storage_mock.url.return_value = '/tmp/test1.ged'
+
+        with mock.patch('django.core.files.storage.default_storage._wrapped', storage_mock):
+            book.save()
+
+        self.assertEqual(book.__str__(), book.title)
