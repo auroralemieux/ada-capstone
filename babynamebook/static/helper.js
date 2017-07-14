56e61c1f3440170a2880c47fbbeb8f677737469a
Aurora Lemieux <aurora.lemieux@gmail.com>
2017-07-13 23:21:45 -0700

    added working progress bar to upload page

--- babynamebook/static/helper.js
+++ babynamebook/static/helper.js
@@ -7,5 +7,43 @@ $(document).ready(function() {
   $('#collapse-all').on('click', function () {
     $('#accordion .panel-collapse').collapse('hide');
   });
-  
+
+
+
+  $('#upload-submit-button').on('click', function() {
+    console.log("clicked submit");
+    $('#percent').html('0%');
+    var progressbar = $('#progressbar');
+    var max = progressbar.attr('aria-valuemax');
+    var time = (1000/max*5);
+    var value = progressbar.attr('aria-valuenow');
+    console.log(max);
+    console.log(time);
+    console.log(value);
+    var loading = function() {
+      console.log("inside loading function");
+      // newValue = value + 1;
+      // console.log(newValue);
+      value = Number(value) + 1;
+      // addValue = progressbar.val(value);
+      progressbar.attr('aria-valuenow', value);
+      newWidth = "width: " + value + "%";
+      progressbar.attr('style', newWidth);
+      $('#percent').html(value + '%');
+
+      if (value == max) {
+        console.log("reached max");
+        clearInterval(animateProgress);
+        $(".progress-bar").addClass("progress-bar-animated");
+      }
+
+    };
+    var animateProgress = setInterval(function() {
+      console.log("inside animate Progress function");
+      loading();
+    }, time);
+
+  animateProgress();
+  });
+
 });
