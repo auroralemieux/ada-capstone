f4508df2b3e1afb3ea3ae12805b3b2aa26531fd9
Aurora Lemieux <aurora.lemieux@gmail.com>
2017-07-13 18:36:36 -0700

    messing around with ged file validation

--- babynamebook/forms.py
+++ babynamebook/forms.py
@@ -1,4 +1,4 @@
-
+# import magic
 from django.core.exceptions import ValidationError
 from django import forms
 
@@ -10,24 +10,24 @@ class BookForm(forms.ModelForm):
         model = Book
         fields = ('title', 'tree_upload',)
 
-    def clean_tree_upload(self):
-        file = self.cleaned_data['tree_upload']
-        try:
-            if file:
-                file_type = file.content_type.split('/')[1]
-                print(file_type)
-                print(file.name)
-
-                if len(file.name.split('.')) == 1:
-                    raise forms.ValidationError(_('File type is not supported'))
-
-                if file_type not in settings.TASK_UPLOAD_FILE_TYPES:
-                    raise forms.ValidationError(_('File type is not supported'))
-        except:
-            pass
-
-        return file
-    # def clean_file(self):
+    # def clean_tree_upload(self):
+    #     file = self.cleaned_data['tree_upload']
+    #     try:
+    #         if file:
+    #             file_type = file.content_type.split('/')[1]
+    #             print(file_type)
+    #             print(file.name)
+    #
+    #             if len(file.name.split('.')) == 1:
+    #                 raise forms.ValidationError(_('File type is not supported'))
+    #
+    #             if file_type not in settings.TASK_UPLOAD_FILE_TYPES:
+    #                 raise forms.ValidationError(_('File type is not supported'))
+    #     except:
+    #         pass
+    #
+    #     return file
+    # def clean_tree_upload(self):
     #     yourfile = self.cleaned_data.get("tree_upload", False)
     #     filetype = magic.from_buffer(yourfile.read())
     #     if not "application/ged" in filetype:
