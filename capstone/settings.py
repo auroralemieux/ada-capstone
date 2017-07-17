ed0004f2ded3e5aaa53630ca168cb98682e1d321
Aurora Lemieux <aurora.lemieux@gmail.com>
2017-07-17 13:16:27 -0700

    added allowed host test url

--- capstone/settings.py
+++ capstone/settings.py
@@ -29,7 +29,7 @@ DEBUG = True
 
 # ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'auroraanderson.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'test-babynamebook.us-west-2.elasticbeanstalk.com']
 
 
 # Application definition
