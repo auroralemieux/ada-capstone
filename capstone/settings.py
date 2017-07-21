b1fad6125ecb4e24129ab8125120b2fba53c5653
Aurora Lemieux <aurora.lemieux@gmail.com>
2017-07-20 23:36:02 -0700

    installed django-extensions and created an erd for project, some search styling

--- capstone/settings.py
+++ capstone/settings.py
@@ -57,6 +57,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',
     'babynamebook',
     'coverage',
     'django_nose',
