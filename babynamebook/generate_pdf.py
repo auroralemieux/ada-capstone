961113b9494e9d42b9d4ff2788b767c57f13202c
Aurora Lemieux <aurora.lemieux@gmail.com>
2017-07-21 13:56:41 -0700

    more static

--- babynamebook/generate_pdf.py
+++ babynamebook/generate_pdf.py
@@ -36,7 +36,7 @@ def go(book_data):
     smallCentered = ParagraphStyle(name="centeredStyle", fontSize=12, alignment=TA_CENTER)
     medCentered = ParagraphStyle(name="centeredStyle", fontSize=14, alignment=TA_CENTER)
 
-    doc = SimpleDocTemplate("media/babynamebook.pdf")
+    doc = SimpleDocTemplate("babynamebook.pdf")
     Story = [Spacer(1,2*inch)]
     # Story.append(Spacer(PAGE_WIDTH/2.0, PAGE_HEIGHT/2.0))
     Story.append(Paragraph("%s" % (book_data["title"]), title_style))
