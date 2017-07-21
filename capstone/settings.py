465516c32e249158c0c16006e2dc2315d20b7897
Aurora Lemieux <aurora.lemieux@gmail.com>
2017-07-21 10:49:20 -0700

    removed django extensions from settings

--- capstone/settings.py
+++ capstone/settings.py
@@ -57,7 +57,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_extensions',
     'babynamebook',
     'coverage',
     'django_nose',
