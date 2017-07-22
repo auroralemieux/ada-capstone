6f922fd5c6c5c3ac6b1bf3ccfe79b48b649d6aaa
Aurora Lemieux <aurora.lemieux@gmail.com>
2017-07-22 10:09:54 -0700

    changed location of file uploadto

--- babynamebook/models.py
+++ babynamebook/models.py
@@ -35,7 +35,7 @@ class Book(models.Model):
 
     author = models.ForeignKey(User)
     names = models.ManyToManyField(Name)
-    tree_upload = models.FileField(upload_to='uploads', max_length=200)
+    tree_upload = models.FileField(upload_to='media', max_length=200)
     title = models.CharField(max_length=200)
     created_date = models.DateTimeField(default=timezone.now)
 
