ead77ccedf3073b2cb725eaac7f7d3cd71f6dd15
Aurora Lemieux <aurora.lemieux@gmail.com>
2017-07-23 23:29:58 -0700

    trying new config of static files

--- babynamebook/static/helper.js
+++ babynamebook/static/helper.js
@@ -44,10 +44,10 @@ $(document).ready(function() {
       if (event.handled !== true) {
         var toggleHeart = function() {
           console.log("toggling heart");
-          if ($(whichHeart).attr('src') == "https://babynamebooktestbucket.s3.amazonaws.com/static/blue-heart-icon.png") {
-            $(whichHeart).attr("src", "https://babynamebooktestbucket.s3.amazonaws.com/static/plain-heart.jpg");
+          if (($(whichHeart).attr('src') == "../../static/blue-heart-icon.png") || ($(whichHeart).attr('src') == "https://babynamebooktestbucket.s3.amazonaws.com/static/blue-heart-icon.png")) {
+            $(whichHeart).attr("src", "../../static/plain-heart.jpg");
           } else {
-            $(whichHeart).attr("src", "https://babynamebooktestbucket.s3.amazonaws.com/static/blue-heart-icon.png");
+            $(whichHeart).attr("src", "../../static/blue-heart-icon.png");
           }
         };
         toggleHeart();
