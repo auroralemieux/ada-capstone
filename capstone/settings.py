76646c1ed2ee25daf46aece1d6567c70d0ed8891
Aurora Lemieux <aurora.lemieux@gmail.com>
2017-07-25 13:16:48 -0700

    adding htaccess stuff

--- capstone/settings.py
+++ capstone/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 import os
 from decouple import config, Csv
 
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
